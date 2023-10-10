a11350ea4d3d9671816d2e3dbfb4983ace5a40ae
Saulihio1321 <143568343+Saulihio1321@users.noreply.github.com>
2023-10-09 19:55:53 -0600

    Add files via upload

---
+++ EVA2_2_IF_ANIDADO2/src/eva2_2_if_anidado2/EVA2_2_IF_ANIDADO2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_2_if_anidado2;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_2_IF_ANIDADO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
